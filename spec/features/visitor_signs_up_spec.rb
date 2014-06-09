46c83699cf4f3ca42e146d9af07993a3b68660c9
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-06-08 21:09:44 -0300

    Added Facebook and Twitter sign up specs

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,10 +1,24 @@
 require 'oauth_helper'
 
-feature 'Visitor signs up' do
+feature 'Visitor should be able to signs up' do
   scenario 'with facebook' do
     visit(new_user_session_path)
     click_link('Sign in with Facebook')
 
     expect(page).to have_content('Autorizado com sucesso de uma conta de Facebook.')
+  end
+
+  scenario 'with twitter' do
+    visit(new_user_session_path)
+    click_link('Sign in with Twitter')
+
+    expect(page).to have_content('Autorizado com sucesso de uma conta de Twitter.')
+  end
+
+  scenario 'with github' do
+    visit(new_user_session_path)
+    click_link('Sign in with Github')
+
+    expect(page).to have_content('Autorizado com sucesso de uma conta de Github.')
   end
 end
--- spec/oauth_helper.rb
+++ spec/oauth_helper.rb
@@ -14,3 +14,29 @@ OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
                                                                       ]
                                                                   }
                                                               })
+
+OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+                                                                 credentials: {
+                                                                     token: "big_token"
+                                                                 },
+                                                                 nickname: "Paulo Henrique Lopes Ribeiro",
+                                                                 info: {
+                                                                     urls: [
+                                                                         "https://www.facebook.com/app_scoped_user_id/297654497066791/"
+                                                                     ]
+                                                                 }
+                                                             })
+
+OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+                                                                credentials: {
+                                                                    token: "big_token"
+                                                                },
+                                                                email: "plribeiro3000@gmail.com",
+                                                                nickname: "Paulo Henrique Lopes Ribeiro",
+                                                                info: {
+                                                                    email: "plribeiro3000@gmail.com",
+                                                                    urls: [
+                                                                        "https://www.facebook.com/app_scoped_user_id/297654497066791/"
+                                                                    ]
+                                                                }
+                                                            })
