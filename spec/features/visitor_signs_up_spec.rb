735cd25944b593ebc284eb75ea8b42521310022c
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-06-09 17:48:12 -0300

    Translating views to portuguese and adding support to add another ones

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -3,28 +3,28 @@ require 'oauth_helper'
 feature 'Visitor should be able to signs up' do
   scenario 'with facebook' do
     visit(new_user_session_path)
-    click_link('Sign in with Facebook')
+    click_link('Entrar com Facebook')
 
     expect(page).to have_content('Autorizado com sucesso de uma conta de Facebook.')
   end
 
   scenario 'with twitter' do
     visit(new_user_session_path)
-    click_link('Sign in with Twitter')
+    click_link('Entrar com Twitter')
 
     expect(page).to have_content('Autorizado com sucesso de uma conta de Twitter.')
   end
 
   scenario 'with github' do
     visit(new_user_session_path)
-    click_link('Sign in with Github')
+    click_link('Entrar com Github')
 
     expect(page).to have_content('Autorizado com sucesso de uma conta de Github.')
   end
 
   scenario 'with linkedin' do
     visit(new_user_session_path)
-    click_link('Sign in with Linkedin')
+    click_link('Entrar com Linkedin')
 
     expect(page).to have_content('Autorizado com sucesso de uma conta de Linkedin.')
   end
