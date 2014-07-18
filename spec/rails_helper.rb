43b9ba5e826be7dc38241675628f26d64e6784de
Ivan Santos <pragmaticivan@gmail.com>
2014-07-18 19:42:45 -0300

    Adicionando spec_helper.rb novamente

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
