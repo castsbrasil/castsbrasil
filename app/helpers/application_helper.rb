4507672ec318c0aa601922b60f0e997a6b6202fa
Rafael Fragoso <rafaelfragosom@gmail.com>
2014-08-01 10:38:30 -0300

    Created 2 helpers to stract the controller and action name to a string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def _controller_name
+    params[:controller].gsub('/', ' ').titleize.gsub(' ', '.').to_s 
+  end
+
+  def _action_name
+    action_name.capitalize
+  end
 end
