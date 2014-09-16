1db6a46bbafd0eefcd07bf0664be5cdd0e2232a6
Thiago Ramos <thiagoramos.al@gmail.com>
2014-09-16 20:47:41 -0300

    Finalizando o Crud de Categoria

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,43 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!, except: %i(index show)
+  before_action :set_category, only: %i(show edit update destroy)
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @category = Category.create(category_params)
+    respond_with @category
+  end
+
+  def update
+    @category.update(category_params)
+    respond_with @category
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+end
