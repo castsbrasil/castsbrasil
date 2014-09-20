69b5ccb4cae6406e7e65b963650dcfa0539e9c9d
Gabriel Caires <gabrielcairesmar@gmail.com>
2014-09-20 17:47:39 -0400

    flow gamefication

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140916222939) do
+
   create_table "authorizations", force: true do |t|
     t.integer  "user_id"
     t.string   "nick_name"
@@ -35,17 +36,18 @@ ActiveRecord::Schema.define(version: 20140916222939) do
 
   add_index "casts", ["user_id"], name: "index_casts_on_user_id"
 
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "levels", force: true do |t|
     t.string   "name"
     t.integer  "required_score"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id"
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "links", force: true do |t|
@@ -57,6 +59,16 @@ ActiveRecord::Schema.define(version: 20140916222939) do
   end
 
   add_index "links", ["profile_id"], name: "index_links_on_profile_id"
+
+  create_table "points", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "value",       null: false
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "points", ["user_id"], name: "index_points_on_user_id"
 
   create_table "profiles", force: true do |t|
     t.string   "first_name"
