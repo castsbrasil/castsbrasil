92b91b8ff8cb29c243e9a781765229c097e90038
Luiz Cezer <lccezinha@gmail.com>
2014-10-23 22:05:07 -0200

    Adicionando Imagem do Gravatar ao registrar por email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def show_avatar(current_user)
+    default_avatar = "http://lorempixel.com/26/26"
+    gravatar       = Gravatar.new(current_user.email)
+
+    avatar = gravatar.exists? ? gravatar.image_url : default_avatar
+
+    image_tag(avatar)
+  end
+
 end
