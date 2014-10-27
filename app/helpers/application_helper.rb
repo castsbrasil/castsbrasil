8b6251fb48292858762d26d513f031922c8a0204
Luiz Cezer <lccezinha@gmail.com>
2014-10-27 00:03:27 -0200

    Fix helper to show avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
 
   def show_avatar(current_user)
-    default_avatar = "http://lorempixel.com/26/26"
-    gravatar       = Gravatar.new(current_user.email)
+    avatar = if current_user.authorizations.github.present?
+      current_user.authorizations.github.avatar_url
+    else
+      default_avatar = "http://lorempixel.com/26/26/"
+      gravatar = Gravatar.new(current_user.email)
+      gravatar.exists? ? gravatar.url : default_avatar
+    end
 
-    avatar = gravatar.exists? ? gravatar.image_url : default_avatar
-
-    image_tag(avatar)
+    image_tag(avatar, class: 'avatar')
   end
 
 end
