45c598a11d4a687420af92484807eb8483aa63f9
Eduardo Maia <eduvimaia@gmail.com>
2014-11-24 21:44:30 -0200

    criado retorno da listagem de screencasts no repositório de casts

--- app/services/cast_repository.rb
+++ app/services/cast_repository.rb
@@ -1,9 +1,15 @@
 class CastRepository
-  attr_reader :most_recent, :most_viewed, :all_screencasts
+  attr_reader :most_recent, :most_viewed, :screencasts_list
 
   def get_home_collections
     @most_recent = Cast.most_recent
     @most_viewed = Cast.none
+
+    self
+  end
+
+  def get_screencasts_collections
+    @screencasts_list = Cast.all.desc
 
     self
   end
