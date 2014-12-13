4c81c74bc5dd1e49ca30e9164729764aacbb2dd0
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-12-13 19:31:52 -0200

    Mais Refatorações

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :casts
   resource :profiles, only: [:update]
-  resources :categories
+  resources :screencasts, only: %i(index show)
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: :registrations
   devise_scope :user do
     resource :registration,
-             only: [:new, :create, :edit, :update],
+             only: %i(new create edit update),
              path: 'users',
              path_names: { new: 'sign_up' },
              controller: 'devise/registrations',
              as: :user_registration
   end
-  get '/' => 'home#index', as: :users  # TODO: lol users_url ? Oo
+
   get '/colabore', to: redirect('https://github.com/RubyCastsBrasil/RubyCastsBrasil/wiki/Contribuindo'), as: :contributing
   get '/sobre', to: redirect('https://github.com/RubyCastsBrasil/RubyCastsBrasil/wiki'), as: :about
-  scope '/screencasts' do
-    get '/' => 'home#screencasts', as: :screencasts
-    get '/:id' => 'home#screencast', as: :screencast
-  end
+
+  get '/' => 'home#index', as: :users
   root to: 'home#index'
 end
