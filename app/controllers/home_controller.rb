07b8a97adcbb070ed6f627d363c41877b4dd3977
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-12-14 13:50:16 -0200

    Alguns acertos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,4 @@ class HomeController < ApplicationController
   def index
     @casts = CastCollection.filtered
   end
-
-  def profile
-    @casts = CastRepository.new.get_home_collections
-  end
 end
